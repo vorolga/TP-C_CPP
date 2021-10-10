8fad3528d10e26f58328d8df1437c8a70198c9cc
vorolga <voronovaolja2001@mail.ru>
2021-10-11 00:48:08 +0300

    testing

--- tp_iz1/include/geometry.h
+++ tp_iz1/include/geometry.h
@@ -2,21 +2,21 @@
 #define TP_IZ1_GEOMETRY_H
 
 struct Circle {
-    double radius;
+  double radius;
 };
 
 struct Triangle {
-    double a;
-    double b;
-    double c;
+  double a;
+  double b;
+  double c;
 };
 
 struct Square {
-    double side;
+  double side;
 };
 
 double find_circle_radius(double, double, double);
 
-//double find_square_side(double);
+// double find_square_side(double);
 
-#endif //TP_IZ1_GEOMETRY_H
+#endif // TP_IZ1_GEOMETRY_H
--- tp_iz1/src/geometry.c
+++ tp_iz1/src/geometry.c
@@ -2,11 +2,11 @@
 #include <math.h>
 
 double find_circle_radius(double a, double b, double c) {
-    double p = (a + b + c) / 2;
-    double triangle_square = sqrt(p * (p - a) * (p - b) * (p - c));
-    return a * b * c / (4 * triangle_square);
+  double p = (a + b + c) / 2;
+  double triangle_square = sqrt(p * (p - a) * (p - b) * (p - c));
+  return a * b * c / (4 * triangle_square);
 }
 
-//double find_square_side(double radius) {
+// double find_square_side(double radius) {
 //    return 2 * radius;
 //}
