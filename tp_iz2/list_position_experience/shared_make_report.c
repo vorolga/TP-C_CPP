01bb72f634e1bd75b6b7c4d443cd198bacbe3ce3
vorolga <voronovaolja2001@mail.ru>
2021-11-09 18:52:02 +0300

    fix

--- tp_iz2/list_position_experience/shared_make_report.c
+++ tp_iz2/list_position_experience/shared_make_report.c
@@ -81,7 +81,6 @@ int make_report(const Node* head)
 
         FILE* fp = fdopen(fd[0], "rb");
         int sums[number_of_processors];
-        printf("%ld ", fread(sums, sizeof(*sums), number_of_processors, fp));
         if (fp && fread(sums, sizeof(*sums), number_of_processors, fp) == (size_t)number_of_processors) {
             double avg_salary = (double)get_sum_int(sums, number_of_processors) / (double)node->employeeArr->real_size;
             printf("Position: %-10s   Experience: %-10s   Average salary: %f\n", node->position, node->experience,
