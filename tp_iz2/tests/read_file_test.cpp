9be46228ac8cb38557bf983488f1f97f6c169e69
vorolga <voronovaolja2001@mail.ru>
2021-11-09 16:47:38 +0300

    after changes in pr

--- tp_iz2/tests/read_file_test.cpp
+++ tp_iz2/tests/read_file_test.cpp
@@ -9,7 +9,7 @@ TEST(READ_FILE_TEST, wrong_filename)
     char filename[30] = "file";
     Node* list_position_experience = NULL;
 
-    EXPECT_EQ(read_file(&list_position_experience, filename), 0);
+    EXPECT_EQ(read_file(&list_position_experience, filename), 1);
 
     if (list_position_experience) {
         free_node(&list_position_experience);
